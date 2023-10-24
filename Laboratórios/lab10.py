21b69c9f3d4e31f54d6f60f806792ed75308c677
Pedro Henrique Martins Belo <pedrohenriqueufpa2018@gmail.com>
2023-10-24 15:17:05 -0300

    ATUALIZAÇÂO!

--- Laboratórios/lab10.py
+++ Laboratórios/lab10.py
@@ -5,6 +5,26 @@
 # RA: 267809
 #####################################################
 
+#Você está desenvolvendo um jogo sobre caçadores de tesouros. Em uma das fases do jogo, cada jogador controla um personagem e deve coletar os tesouros que encontrar pelo mapa. Porém, como os tesouros são pesados, cada personagem consegue carregar apenas um tesouro por vez.
+
+#Durante a caça aos tesouros, um personagem por vez caminhará pelo mapa. Ao encontrar um tesouro pela primeira vez, o personagem irá carregá-lo junto com ele, removendo o tesouro daquele local. Caso encontre um segundo tesouro enquanto já estiver carregando um tesouro, se este tesouro encontrado possuir um valor maior do que o está sendo carregado, ele substituirá os dois tesouros, ou seja, jogará o seu tesouro de menor valor naquela posição do mapa e carregará o tesouro de maior valor consigo. Após o primeiro personagem terminar seu caminho no mapa, o segundo personagem irá percorrê-lo, e assim por diante.
+
+#Como você gosta de antever os resultados da partida, você resolveu criar um programa para indicar o valor que cada personagem irá coletar.
+
+#Como entrada o seu programa, você deverá ler dois inteiros, n e m, que indicam a quantidade de linhas e colunas do mapa, respectivamente. Nas próximas n linhas, o seu código receberá as linhas do mapa, com valores inteiros entre 0 e 9, sendo que 0 indica uma posição sem tesouro e valores maiores que 0 representam o valor do tesouro naquela posição. Na sequência, você deverá ler o inteiro q, indicando a quantidade de personagens. Para cada personagem, você deverá ler duas linhas, sendo a primeira linha com dois valores inteiros, representando a linha e a coluna inicial do personagem (por exemplo, a posição no canto superior esquerdo do mapa é denotada por 0 0), e a segunda linha representando os movimentos realizados pelo personagem. Os seguintes caracteres são utilizados para representar os movimentos:
+
+#N: representa uma movimentação para o norte, ou seja para a linha acima da posição atual.
+#S: representa uma movimentação para o sul, ou seja para a linha abaixo da posição atual.
+#O: representa uma movimentação para o oeste, ou seja para a coluna à esquerda da posição atual.
+#L: representa uma movimentação para o leste, ou seja para a coluna à direita da posição atual.
+#Você pode considerar que todos os movimentos são válidos, sempre mantendo o personagem dentro do mapa.
+
+#Como saída o seu programa deve imprimir o valor do tesouro encontrado para cada um dos personagens. Caso o personagem não encontre nenhum tesouro, o valor deverá ser igual a 0. A seguinte mensagem deve ser impressa para cada caçador, sendo C o número do caçador e T o valor do tesouro:
+
+#Caçador C: T
+
+#Resolução:
+
 def seguinte(l,c,letra, mapa):
     if letra == 'N':
         l -= 1
--- Laboratórios/lab11.py
+++ Laboratórios/lab11.py
@@ -1,9 +1,21 @@
 ###################################################
 # MC102 - Algoritmos e Programação de Computadores
 # Laboratório 11 - Loteamento
-# Nome: 
-# RA: 
+# Nome: Pedro Henrique Martins Belo
+# RA: 267809
 ###################################################
+
+#Seu amigo possui um grande terreno e planeja realizar um loteamento, vendendo parcelas menores para cada comprador. Como a tarefa de gerenciar os lotes não é trivial, ele pediu para você criar um programa para auxiliar no gerenciamento das vendas.
+
+#O terreno original possui 15 unidades de largura e 15 unidades de comprimento. Como a área será habitada, seu amigo irá criar ruas que irão atravessar a área na vertical e na horizontal. Além disso, ele venderá o restante da terra para compradores, que indicarão a porção de terra que cada um deles tem interesse.
+
+#Seu programa deverá ler duas linhas, com as informações das ruas. A primeira linha possuirá um ou mais valores inteiros indicando os índices na matriz em que as ruas horizontais serão criadas e a segunda linha possuirá um ou mais valores inteiros representando os índices na matriz em que as ruas verticais serão criadas. Na próxima linha, seu código receberá um valor inteiro n, indicando a quantidade de compradores, sendo que n será menor ou igual a 9. Nas próximas n linhas, você deverá ler quatro inteiros, sendo os dois primeiros representando a linha e a coluna do canto superior esquerdo do lote, enquanto os dois últimos representam a linha e a coluna do canto inferior direito do lote de interesse do comprador, sendo que o lote sempre terá formato retangular. O canto superior esquerdo do loteamento é indicado como linha 0 e coluna 0. Caso a proposta de compra conflite com um lote já vendido ou com uma rua, aquela compra não será realizada.
+
+#Como saída, seu código deverá imprimir o mapa do terreno, sendo as ruas representadas pelo caracter ., e os terrenos vendidos representados pelo número do comprador (começando de 1 e indo até 9). Pedaços do terreno não vendidos deverão ser representadas por x.
+
+#Exemplos de entradas e saídas esperadas pelo seu programa:
+
+#Resolução:
 
 loteamento = []
 for i in range(0, 15):
